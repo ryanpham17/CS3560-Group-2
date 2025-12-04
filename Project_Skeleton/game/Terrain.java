dc7a9104f665cf061db4ca40287528f040fc8dfd
emorcos0 <edwardmorcos2004@gmail.com>
2025-12-03 17:10:54 -0800

    Difficulty now influences spawns.
    Difficulty now affects terrain spawn types.

--- Project_Skeleton/game/Terrain.java
+++ Project_Skeleton/game/Terrain.java
@@ -10,17 +10,50 @@ public abstract class Terrain
     public abstract int waterCost();
     public abstract String name();
 
-    // Generation of random terrain. Takes difficulty, but currently does not use it for generation.
+    // Generation of random terrain. Takes difficulty to determine generation.
     public static Terrain randomTerrain(int difficulty) 
     {
         Random r = new Random();
-        int x = r.nextInt(5);
-        return switch (x) {
-            case 0 -> new Plains();
-            //case 1 -> new Forest();
-            //case 2 -> new Desert();
-            //case 3 -> new Mountain();
-            default -> new Plains();
-        };
+        int x = r.nextInt(10);
+
+        // More plains and forests on easy.
+        if(difficulty == 1)
+        {
+            return switch (x) 
+            {
+                case 0,1,2 -> new Plains();
+                case 4,5 -> new Forest();
+                case 6,7 -> new Desert();
+                case 8 -> new Mountain();
+                case 9 -> new Swamp();
+                default -> new Plains();
+            };
+        }
+
+        // More Deserts and Mountanis and medium.
+        else if (difficulty == 2)
+        {
+            return switch (x) 
+            {
+                case 0,1 -> new Plains();
+                case 2,3 -> new Forest();
+                case 4,5,6 -> new Desert();
+                case 7,8 -> new Mountain();
+                case 9 -> new Swamp();
+                default -> new Plains();
+            };
+        }
+
+        // Even more deserts, mountains, and swamps on hard.
+        return switch (x) 
+            {
+                case 0,1 -> new Plains();
+                case 2 -> new Forest();
+                case 3,4,5 -> new Desert();
+                case 6,7,8-> new Mountain();
+                case 9 -> new Swamp();
+                default -> new Plains();
+            };
+
     }
 }
